6e7cf5f5de8911e4b29997153b72f9a668a5804e
Moe <5617340+mo2@user.noreply.gitee.com>
2020-02-18 00:19:59 +0000

    fix bug

--- installDebian.sh
+++ installDebian.sh
@@ -303,9 +303,9 @@ cat >/data/data/com.termux/files/usr/bin/debian-i <<-'EndOfFile'
     fi
 
 	if [ ! -e $PREFIX/bin/wget ]; then
-		apt update;apt install -y wget ; wget -qO /data/data/com.termux/files/usr/etc/storage/DebianManager.sh >/dev/null 2>&1 && bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash || bash /data/data/com.termux/files/usr/etc/storage/DebianManager.bash  || bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash
+		apt update;apt install -y wget ; wget -qO /data/data/com.termux/files/usr/etc/storage/DebianManager.sh >/dev/null 2>&1 && bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash >/dev/null 2>&1 || bash /data/data/com.termux/files/usr/etc/storage/DebianManager.bash >/dev/null 2>&1  || bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash >/dev/null 2>&1 
 	else
-	wget -qO /data/data/com.termux/files/usr/etc/storage/DebianManager.sh >/dev/null 2>&1 && bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash || bash /data/data/com.termux/files/usr/etc/storage/DebianManager.bash|| bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash
+	wget -qO /data/data/com.termux/files/usr/etc/storage/DebianManager.sh >/dev/null 2>&1 && bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash >/dev/null 2>&1 || bash /data/data/com.termux/files/usr/etc/storage/DebianManager.bash >/dev/null 2>&1  || bash /data/data/com.termux/files/usr/etc/storage/DebianManagerLatest.bash >/dev/null 2>&1 
 	fi	
 
 EndOfFile
