0d3e43cf190d3e8f96e7b9024e892e19a9510d4e
Moe <5617340+mo2@user.noreply.gitee.com>
2020-02-21 07:37:15 +0800

    --allow-overwrite=true

--- debian.sh
+++ debian.sh
@@ -853,7 +853,7 @@ fi
 ########################################################################
 UPDATEMANAGER(){
 
-aria2c -d $PREFIX/bin -o debian-i 'https://gitee.com/mo2/Termux-Debian/raw/master/debian.sh'
+aria2c --allow-overwrite=true -d $PREFIX/bin -o debian-i 'https://gitee.com/mo2/Termux-Debian/raw/master/debian.sh'
 #wget -qO $PREFIX/bin/debian-i 'https://gitee.com/mo2/Termux-Debian/raw/master/debian.sh'
 echo "${YELLOW}更新完成，按回车键返回。${RESET}"
 echo 'Press enter to return.'
@@ -873,7 +873,7 @@ DOWNLOADVNCAPK(){
 		rm -f 'VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz'  2>/dev/null
 		echo '正在为您下载至/sdcard/Download目录...'
 		echo 'Download size 11.1MB'
-         aria2c -x 16 -k 1M --split 16 'https://cdn.tmoe.me/git/Termux-Debian/VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz' || aria2c -x 16 -k 1M --split 16 'https://m.tmoe.me/down/share/Android/VNC/VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz'        	
+         aria2c -x 16 -k 1M --split=16 --allow-overwrite=true 'https://cdn.tmoe.me/git/Termux-Debian/VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz' || aria2c -x 16 -k 1M --split 16 'https://m.tmoe.me/down/share/Android/VNC/VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz'        	
 		echo '正在解压...'
 	    tar -Jxvf 'VNCViewer_com-realvnc-viewer-android-3-6-1-42089.tar.xz'
         echo '正在删除压缩包...'
