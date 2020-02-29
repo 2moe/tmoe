ff3f75d3c41c05896deb823f09ac03e7240786a3
Moe <5617340+mo2@user.noreply.gitee.com>
2020-02-29 08:45:06 +0800

    modify title

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -31,7 +31,7 @@ CHECKdependencies() {
 }
 ####################################################
 DEBIANMENU() {
-	OPTION=$(whiptail --title "Tmoe-Debian Tool输debian-i启动本工具(20200229)" --menu "Type 'debian-i' to start this tool.Please use the arrow and enter key to operate.当前主菜单有十几个选项，请使用方向键或触屏上下滑动，按回车键确认。" 15 50 4 \
+	OPTION=$(whiptail --title "Tmoe-Debian Tool输debian-i启动(20200229)" --menu "Type 'debian-i' to start this tool.Please use the arrow and enter key to operate.当前主菜单有十几个选项，请使用方向键或触屏上下滑动，按回车键确认。" 15 50 4 \
 		"1" "Install GUI 安装图形界面" \
 		"2" "Install browser 安装浏览器" \
 		"3" "Remove GUI 卸载图形界面" \
