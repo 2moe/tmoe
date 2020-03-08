222042668975a5cbeb5ed164fc0b5e5f021a69eb
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-08 22:52:22 +0800

    modify title

--- debian.sh
+++ debian.sh
@@ -224,7 +224,7 @@ ANDROIDTERMUX() {
 
 MainMenu() {
 	OPTION=$(
-		whiptail --title "Tmoe-Debian manager running on Termux(20200308)" --backtitle "$(
+		whiptail --title "Tmoe-Debian GNU/Linux manager(20200308)" --backtitle "$(
 			base64 -d <<-'DoYouWantToSeeWhatIsInside'
 				6L6TZGViaWFuLWnlkK/liqjmnKznqIvluo8sVHlwZSBkZWJpYW4taSB0byBzdGFydCB0aGUgdG9v
 				bCzokIzns7vnlJ/niannoJTnqbblkZgK
