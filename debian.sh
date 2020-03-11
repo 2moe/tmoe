95c421cef523f08e180b9a2b94f8b793a7f7c940
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-11 11:58:47 +0800

    优化xz压缩方式

--- debian.sh
+++ debian.sh
@@ -571,7 +571,9 @@ BackupSystem() {
 			echo "您选择了tar.xz,即将为您备份至/sdcard/Download/backup/${TMPtime}.tar.xz"
 			echo "${YELLOW}按回车键开始备份,按Ctrl+C取消。Press Enter to start the backup.${RESET} "
 			read
-			tar -PJpcf - --exclude=~/${DebianFolder}/root/sd --exclude=~/${DebianFolder}/root/tf --exclude=~/${DebianFolder}/root/termux ~/${DebianFolder} $PREFIX/bin/debian | (pv -p --timer --rate --bytes >${TMPtime}.tar.xz)
+           tar -Ppcvf ${TMPtime}.tar --exclude=${DebianCHROOT}/root/sd --exclude=${DebianCHROOT}/root/tf --exclude=${DebianCHROOT}/root/termux ${DebianCHROOT} $PREFIX/bin/debian
+		   xz -z -T0 -e -9 -v -f ${TMPtime}.tar
+			#tar -PJpcf - --exclude=~/${DebianFolder}/root/sd --exclude=~/${DebianFolder}/root/tf --exclude=~/${DebianFolder}/root/termux ~/${DebianFolder} $PREFIX/bin/debian | (pv -p --timer --rate --bytes >${TMPtime}.tar.xz)
 			#whiptail进度条已弃用
 			#tar -PJpcf - --exclude=~/${DebianFolder}/root/sd --exclude=~/${DebianFolder}/root/tf --exclude=~/${DebianFolder}/root/termux ~/${DebianFolder} $PREFIX/bin/debian | (pv -n >${TMPtime}.tar.xz) 2>&1 | whiptail --gauge "Packaging into tar.xz" 10 70
 
