85242e14820606c97a91359ad1e989c79a1f9de3
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-13 02:37:55 +0800

    fix resolution bug

--- debian.sh
+++ debian.sh
@@ -1570,10 +1570,8 @@ MODIFYANDROIDTERMUXVNCCONF() {
 	CURRENTTERMUXVNCRES=$(sed -n 6p "$(which startvnc)" | cut -d 'y' -f 2 | cut -d '-' -f 1)
 	if (whiptail --title "modify vnc configuration" --yes-button '分辨率resolution' --no-button '其它other' --yesno "您想要修改哪些配置信息？What configuration do you want to modify?" 9 50); then
 		TARGET=$(whiptail --inputbox "Please enter a resolution,请输入分辨率,例如2880x1440,2400x1200,1920x1080,1920x960,1440x720,1280x1024,1280x960,1280x720,1024x768,800x680等等,默认为720x1440,当前为${CURRENTTERMUXVNCRES}。分辨率可自定义，但建议您根据屏幕比例来调整，输入完成后按回车键确认，修改完成后将自动停止VNC服务。注意：x为英文小写，不是乘号。Press Enter after the input is completed." 16 50 --title "请在方框内输入 水平像素x垂直像素 (数字x数字) " 3>&1 1>&2 2>&3)
-		exitstatus=$?
-		if [ $exitstatus = 0 ]; then
-			#sed -i "s:${CURRENTTERMUXVNCRES}:${TARGET}:g" "$(which startvnc)"
-			sed -i "6 c vncserver -geometry $TARGET --SecurityTypes=None \$DISPLAY &" "$(which startvnc)"
+			sed -i "s:${CURRENTTERMUXVNCRES}:${TARGET}:g" "$(which startvnc)"
+			#sed -i "6 c vncserver -geometry $TARGET --SecurityTypes=None \$DISPLAY &" "$(which startvnc)"
 			echo 'Your current resolution has been modified.'
 			echo '您当前的分辨率已经修改为'
 			echo $(sed -n 6p "$(which startvnc)" | cut -d 'y' -f 2 | cut -d '-' -f 1)
@@ -1581,12 +1579,6 @@ MODIFYANDROIDTERMUXVNCCONF() {
 			echo "${YELLOW}按回车键返回。${RESET}"
 			read
 			MainMenu
-
-		else
-			echo '您当前的分辨率为'
-			echo $(sed -n 6p "$(which startvnc)" | cut -d 'y' -f 2 | cut -d '-' -f 1)
-		fi
-
 	else
 		echo '您可以手动修改vnc的配置信息'
 		echo 'If you want to modify the resolution, please change the 720x1440 (default resolution , vertical screen) to another resolution, such as 1920x1080 (landscape).'
