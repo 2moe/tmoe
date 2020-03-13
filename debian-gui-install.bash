5d5ee01966400e305a3f53fd8ad9e10dbfe8a2ba
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-13 22:28:38 +0800

    auto select keyboard layoutcode

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -722,6 +722,9 @@ OTHERSOFTWARE() {
 INSTALLXFCE4DESKTOP() {
 	apt-mark hold udisks2
 	apt update
+	echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+    echo keyboard-configuration keyboard-configuration/layout select 'English (US)' | debconf-set-selections
+    echo keyboard-configuration keyboard-configuration/layoutcode select 'us' | debconf-set-selections
 	echo '即将为您安装思源黑体(中文字体)、xfce4、xfce4-terminal、xfce4-goodies和tightvncserver等软件包。'
 	apt install -y fonts-noto-cjk xfce4 xfce4-terminal xfce4-goodies tightvncserver
 	apt install -y xfwm4-theme-breeze xcursor-themes
