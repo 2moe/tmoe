ea0733caaa35713a05da46783d5d0ebeced31c57
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-14 12:35:38 +0800

    fix bug

--- 新版已废弃的代码.sh
+++ 新版已废弃的代码.sh
@@ -213,3 +213,4 @@ MODIFYVNCORXSDLCONF() {
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 echo keyboard-configuration keyboard-configuration/layout select 'English (US)' | debconf-set-selections
 echo keyboard-configuration keyboard-configuration/layoutcode select 'us' | debconf-set-selections
+
