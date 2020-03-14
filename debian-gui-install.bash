913eb7aa1752d323264d5f46791f2bf7ae5ab1d4
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-14 12:29:47 +0800

    fix bug

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -744,9 +744,9 @@ OTHERSOFTWARE() {
 			rm -f *url *txt
 			rm -rf 迷之物语 斯隆与马克贝尔的谜之物语k73
 			rm -f slymkbr1.zip* mayomonogatari2.zip*
-			echo "安装完成，您需要手动进入'/root/斯隆与马克贝尔的谜之物语'目录加载游戏"
-			echo "如需卸载，请手动输apt purge -y desmume ; rm -rf ~/斯隆与马克贝尔的谜之物语"
 		fi
+		echo "安装完成，您需要手动进入'/root/斯隆与马克贝尔的谜之物语'目录加载游戏"
+		echo "如需卸载，请手动输apt purge -y desmume ; rm -rf ~/斯隆与马克贝尔的谜之物语"
 		desmume ./3782.nds 2>/dev/null &
 		echo 'Press Enter to return.'
 		echo "${YELLOW}按回车键返回。${RESET}"
