e3af5192b2f456dbbfb8df4c0439d58f0377b472
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-16 00:20:21 +0800

    fix win10 theme

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -626,7 +626,8 @@ Installkaliundercover() {
 	if [ -f "/usr/bin/kali-undercover" ]; then
 		echo "检测到您已安装win10主题"
 	else
-		if [ "$(cat /etc/issue | cut -c 1-4)" = "Kali" ]; then
+		#if [ "$(cat /etc/issue | cut -c 1-4)" = "Kali" ]; then
+		if grep 'kali' '/etc/apt/sources.list'; then
 			apt update
 			apt install -y kali-undercover
 		else
