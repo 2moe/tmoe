7477b4a7ace31a48a8687927739ac5a4ad2c839c
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-17 02:56:13 +0800

    add debian-sid

--- installDebian.sh
+++ installDebian.sh
@@ -161,7 +161,7 @@ DebianTarXz="debian-sid-rootfs.tar.xz"
 #if [ "$downloaded" != 1 ];then
 if [ ! -f ${DebianTarXz} ]; then
   echo "正在从清华大学开源镜像站下载容器镜像"
-  echo "Downloading rootfs.tar.xz from Tsinghua University Open Source Mirror Station."
+  echo "Downloading debian-sid-rootfs.tar.xz from Tsinghua University Open Source Mirror Station."
   curl -L "https://mirrors.tuna.tsinghua.edu.cn/lxc-images/images/debian/sid/${archtype}/default/" -o get-date-tmp.html >/dev/null 2>&1
   ttime=$(cat get-date-tmp.html | tail -n2 | head -n1 | cut -d\" -f4)
   rm -f get-date-tmp.html
