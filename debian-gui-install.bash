09699e3321c9a83c84673ae8a88f8b089c9368ee
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-17 17:55:49 +0800

    fix chromium

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -911,12 +911,12 @@ INSTALLXFCE4DESKTOP() {
 		apt install -y kali-linux
 		apt install -y kali-menu
 		apt install -y kali-undercover
+		apt install -y kali-linux-top10
 		apt install -y kali-linux-arm
-		#apt install -y kali-linux-top10
 		apt install -y chromium-l10n
 		sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
 		grep 'chromium' /etc/profile || sed -i '$ a\alias chromium="chromium --no-sandbox"' /etc/profile
-
+		apt search kali-linux
 	fi
 	apt clean
 
