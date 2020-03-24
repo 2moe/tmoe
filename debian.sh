ba45d6814c51e96570237dbb28d64503a27bca11
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-25 00:08:19 +0800

    Remov support for ios-ish

--- debian.sh
+++ debian.sh
@@ -1903,7 +1903,11 @@ INSTALLDEBIANGNULINUXDISTRO() {
 ########################
 BUSTERORSID() {
 	if (whiptail --title "Debian version" --yes-button 'Sid' --no-button 'Buster' --yesno "请选择您需要安装的debian版本，Please select the debian version you need to install.Buster为当前的stable版,sid为unstable。Buster更加稳定且bug较少,但buster的软件包较旧,而sid较新。Buster is more stable and has fewer bugs, but the packages inside the buster software source are older. The sid package is relatively new." 15 50); then
-		wget -qO- 'https://gitee.com/mo2/linux/raw/master/installDebian.sh' | bash - || bash -c "$(curl -fLsS 'https://gitee.com/mo2/linux/raw/master/installDebian.sh')"
+		if [ "${LINUXDISTRO}" != 'iSH' ]; then
+			bash -c "$(curl -fLsS 'https://gitee.com/mo2/linux/raw/master/installDebian.sh')"
+		else
+			wget -qO- 'https://gitee.com/mo2/linux/raw/master/installDebian.sh' | bash
+		fi
 	else
 		bash -c "$(curl -LfsS gitee.com/mo2/linux/raw/master/installDebian.sh | sed 's:/sid:/buster:g' | sed 's:extract z:extract:' | sed 's:-sid:-buster:g' | sed 's@#deb http@deb http@g' | sed 's/.*sid main/#&/')"
 	fi
