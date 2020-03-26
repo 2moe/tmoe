7f9f6b1e916e44ada982878e2aa12ce49d2f6c32
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-26 16:30:24 +0800

    update version

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -58,7 +58,7 @@ CHECKdependencies() {
 DEBIANMENU() {
 	cd ${cur}
 	OPTION=$(
-		whiptail --title "Tmoe-Debian Tool输debian-i启动(20200326-15)" --menu "Type 'debian-i' to start this tool.Please use the enter and arrow keys to operate.当前主菜单有十几个选项，请使用方向键或触屏上下滑动，按回车键确认。0313本次更新加入了游戏的安装支持。" 19 50 7 \
+		whiptail --title "Tmoe-Debian Tool输debian-i启动(20200326-16)" --menu "Type 'debian-i' to start this tool.Please use the enter and arrow keys to operate.当前主菜单有十几个选项，请使用方向键或触屏上下滑动，按回车键确认。0326本次更新在软件商店中加入了度盘和云音乐。" 19 50 7 \
 			"1" "Install GUI 安装图形界面" \
 			"2" "Install browser 安装浏览器" \
 			"3" "Download theme 下载主题" \
