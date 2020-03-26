034effc30c63b48e1abcaea395766310fc3fa03b
Moe <5617340+mo2@user.noreply.gitee.com>
2020-03-26 16:26:50 +0800

    fix WSL check

--- debian.sh
+++ debian.sh
@@ -293,7 +293,7 @@ GNULINUX() {
 	fi
 
 	if [ "${LINUXDISTRO}" = "debian" ]; then
-		if (whiptail --title "您想要对这个小可爱做什么 " --yes-button "安装工具" --no-button "管理工具" --yesno "检测到您使用的是deb系linux ${WSL} 您是想要启动software安装工具，还是system管理工具？ ♪(^∇^*) " 9 50); then
+		if (whiptail --title "您想要对这个小可爱做什么 " --yes-button "安装工具" --no-button "管理工具" --yesno "检测到您使用的是deb系linux ${WSL}您是想要启动software安装工具，还是system管理工具？ ♪(^∇^*) " 9 50); then
 			bash -c "$(wget -qO- https://gitee.com/mo2/linux/raw/master/debian-gui-install.bash)"
 			exit 0
 		fi
