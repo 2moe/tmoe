c65e957ce3121177d7b6c60836ec415fd4bbb1ca
2moe <25324935+2moe@users.noreply.github.com>
2020-05-04 07:31:29 +0800

    modify tips of fedora

--- debian.sh
+++ debian.sh
@@ -2292,7 +2292,7 @@ choose_which_gnu_linux_distro() {
 		elif [ "${archtype}" = 'i386' ]; then
 			echo "Fedora不支持您的架构"
 		else
-			if (whiptail --title "FEDORA VERSION" --yes-button '31' --no-button '32' --yesno "您想要修改安装哪个版本？" 9 50); then
+			if (whiptail --title "FEDORA VERSION" --yes-button '31' --no-button '32' --yesno "您想要安装哪个版本？Which version do you want to install?" 9 50); then
 				bash -c "$(curl -LfsS raw.githubusercontent.com/2moe/tmoe-linux/master/installDebian.sh |
 					sed 's/debian系统/fedora系统/g' |
 					sed 's/debian system/fedora system/g' |
