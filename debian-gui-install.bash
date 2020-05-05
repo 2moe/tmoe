dd408637f95c000645dfc7affdbaa52c2e74acd9
2moe <25324935+2moe@users.noreply.github.com>
2020-05-05 16:20:33 +0800

    modify tips

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -Lvo /usr/local/bin/debian-i 'https://raw.githubusercontent.com/2moe/tmoe-linux/master/tool.sh'
 chmod +x /usr/local/bin/debian-i
+echo "The developer of this tool changed the script name on May 4th, please update it again."
 echo "2020-05-04更换了脚本名称，请再更新一遍"
-echo 'Please update again'
