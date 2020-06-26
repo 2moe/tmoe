5390ec11379badd2d0fd13ef361f5890330b08a0
2moe <25324935+2moe@users.noreply.github.com>
2020-06-26 13:19:41 +0800

    check_current_user_name_and_group

--- tool.sh
+++ tool.sh
@@ -7779,8 +7779,7 @@ configure_startvnc() {
 ###############
 fix_non_root_permissions() {
 	if [ ${HOME} != '/root' ]; then
-		CURRENT_USER_NAME=$(cat /etc/passwd | grep "${HOME}" | awk -F ':' '{print $1}')
-		CURRENT_USER_GROUP=$(cat /etc/passwd | grep "${HOME}" | awk -F ':' '{print $5}' | cut -d ',' -f 1)
+		check_current_user_name_and_group
 		echo "检测到${HOME}目录不为/root，为避免权限问题，正在将${CURRENT_USER_FILE}的权限归属修改为${CURRENT_USER_NAME}用户和${CURRENT_USER_GROUP}用户组"
 		sudo -E chown -R ${CURRENT_USER_NAME}:${CURRENT_USER_GROUP} "${CURRENT_USER_FILE}" 2>/dev/null || su -c "chown -R ${CURRENT_USER_NAME}:${CURRENT_USER_GROUP} ${CURRENT_USER_FILE}" 2>/dev/null
 	fi
