9cd9b803b9022c1e9f5a079ca71c8fccb257b84c
2moe <25324935+2moe@users.noreply.github.com>
2020-07-14 11:59:33 +0800

    del deb-gui shell script

--- debian-gui-install.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ $(command -v curl) ]; then
-    curl -Lvo /usr/local/bin/debian-i 'https://raw.githubusercontent.com/2moe/tmoe-linux/master/tool.sh'
-else
-    wget -O /usr/local/bin/debian-i 'https://raw.githubusercontent.com/2moe/tmoe-linux/master/tool.sh'
-fi
-chmod +x /usr/local/bin/debian-i
-echo "The developer of this tool changed the script name on May 4th, please update it again."
-echo "2020-05-04更换了脚本名称，请再更新一遍"
