13846c6c17d6df70bdb81b628549c3b8bef728a5
2moe <25324935+2moe@users.noreply.github.com>
2020-07-21 17:00:01 +0800

    x-terminal-emulator

--- tool.sh
+++ tool.sh
@@ -2882,8 +2882,6 @@ configure_vnc_xstartup() {
 		export PULSE_SERVER=127.0.0.1
 		if [ \$(command -v x-terminal-emulator) ]; then
 			x-terminal-emulator &
-		else
-			x-terminal-emulator &
 		fi
 		if [ \$(command -v ${REMOTE_DESKTOP_SESSION_01}) ]; then
 			dbus-launch --exit-with-session ${REMOTE_DESKTOP_SESSION_01} &
