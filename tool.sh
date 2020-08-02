79eda11662bf971c33859c2ada372b7f040887a0
2moe <25324935+2moe@users.noreply.github.com>
2020-08-03 05:39:54 +0800

    remove

--- tool.sh
+++ tool.sh
@@ -646,7 +646,7 @@ remove_tmoe_linux_tool(){
 	echo "${RED}rm -rv ${HOME}/.config/tmoe-linux /usr/share/applications/tmoe-linux.desktop /usr/local/etc/tmoe-linux startvnc stopvnc debian-i startx11vnc startxsdl x11vncpasswd .tmoe-linux-qemu startqemu${RESET}"
 	echo "${RED}${PACKAGES_REMOVE_COMMAND} git aria2 pv wget curl less xz-utils newt whiptail${RESET}"
 	do_you_want_to_continue
- 	rm -rfv /usr/share/applications/tmoe-linux.desktop /usr/local/etc/tmoe-linux startvnc stopvnc debian-i startx11vnc sartxsdl x11vncpasswd
+ 	rm -rfv /usr/share/applications/tmoe-linux.desktop /usr/local/etc/tmoe-linux startvnc stopvnc debian-i startx11vnc startxsdl x11vncpasswd
 	${PACKAGES_REMOVE_COMMAND} git aria2 pv wget curl less xz-utils newt whiptail
 	exit 1
 }
