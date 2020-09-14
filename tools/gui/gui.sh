f3e4c93890196389ca213b293d1e35a952ef40f5
2moe <25324935+2moe@users.noreply.github.com>
2020-09-15 01:21:24 +0800

    ubuntu gnome3

--- tools/gui/gui.sh
+++ tools/gui/gui.sh
@@ -1546,7 +1546,7 @@ install_gnome3_desktop() {
     fi
     gnome3_warning
     REMOTE_DESKTOP_SESSION_01='gnome-session'
-    REMOTE_DESKTOP_SESSION_02='x-window-manager'
+    REMOTE_DESKTOP_SESSION_02='gnome-panel'
     DEPENDENCY_01="gnome"
     echo '即将为您安装思源黑体(中文字体)、gnome-session、gnome-menus、gnome-tweak-tool、gnome-shell和tightvncserver等软件包。'
     if [ "${LINUX_DISTRO}" = "debian" ]; then
@@ -1554,7 +1554,10 @@ install_gnome3_desktop() {
         auto_select_keyboard_layout
         #aptitude install -y task-gnome-desktop || apt install -y task-gnome-desktop
         #apt install --no-install-recommends xorg gnome-session gnome-menus gnome-tweak-tool gnome-shell || aptitude install -y gnome-core
-        DEPENDENCY_01='--no-install-recommends xorg gnome-session gnome-menus gnome-tweak-tool gnome-core gnome-shell-extension-dashtodock gnome-shell'
+        case ${DEBIAN_DISTRO} in
+        ubuntu) DEPENDENCY_01='--no-install-recommends xorg gnome-session gnome-menus gnome-tweak-tool gnome-core gnome-shell' ;;
+        *) DEPENDENCY_01='--no-install-recommends xorg gnome-session gnome-menus gnome-tweak-tool gnome-core gnome-shell-extension-dashtodock gnome-shell' ;;
+        esac
         #若不包含gnome-core，则为最简化安装
     elif [ "${LINUX_DISTRO}" = "redhat" ]; then
         #yum groupinstall "GNOME Desktop Environment"
@@ -3459,7 +3462,7 @@ configure_remote_desktop_enviroment() {
     ##############################
     if [ "${BETA_DESKTOP}" == '6' ]; then
         REMOTE_DESKTOP_SESSION_01='gnome-session'
-        REMOTE_DESKTOP_SESSION_02='x-window-manager'
+        REMOTE_DESKTOP_SESSION_02='gnome-panel'
         #configure_remote_gnome3_desktop
     fi
     ##############################
@@ -4206,7 +4209,7 @@ fix_vnc_dbus_launch() {
         elif grep 'gnome-session' ${XSESSION_FILE}; then
             echo "检测您当前的VNC配置为GNOME3，正在将dbus-launch加入至启动脚本中..."
             REMOTE_DESKTOP_SESSION_01='gnome-session'
-            REMOTE_DESKTOP_SESSION_02='x-windows-manager'
+            REMOTE_DESKTOP_SESSION_02='gnome-panel'
         elif grep 'cinnamon' ${XSESSION_FILE}; then
             echo "检测您当前的VNC配置为cinnamon，正在将dbus-launch加入至启动脚本中..."
             REMOTE_DESKTOP_SESSION_01='cinnamon-session'
