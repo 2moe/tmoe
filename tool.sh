b45f39579fa8400597ee229f7b405bb5ec6617f3
2moe <25324935+2moe@users.noreply.github.com>
2020-12-06 06:55:05 +0000

    LOCALHOST ACCESS

--- tool.sh
+++ tool.sh
@@ -97,7 +97,7 @@ check_tmoe_command() {
 	else
 		TMOE_TIPS_01="tmoe t"
 	fi
-	TMOE_TIPS_00="Welcome to tmoe linux tool v1.3501,type ${TMOE_TIPS_01} to start this tool."
+	TMOE_TIPS_00="Welcome to tmoe linux tool v1.3502,type ${TMOE_TIPS_01} to start this tool."
 	#勿改00变量
 }
 #########
