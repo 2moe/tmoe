eb26272ba6b4b0963db96cc06c6b2b1e53caf8c9
2moe <25324935+2moe@users.noreply.github.com>
2021-01-06 12:21:03 +0000

    fedora neofetch (X)

--- zsh.sh
+++ zsh.sh
@@ -108,11 +108,12 @@ install_lolcat_and_neofetch() {
 		elif [[ -n $(command -v pacman) ]]; then
 			printf "%s\n" "${GREEN}pacman ${YELLOW}-Sy --noconfirm ${BLUE}${i}${RESET}"
 			pacman -Sy --noconfirm ${i}
-		elif [[ -n $(command -v dnf) ]]; then
-			printf "%s\n" "${GREEN}dnf ${YELLOW}install -y ${BLUE}${i}${RESET}"
-			dnf install -y ${i}
+		#elif [[ -n $(command -v dnf) ]]; then
+		#	printf "%s\n" "${GREEN}dnf ${YELLOW}install -y ${BLUE}${i}${RESET}"
+		#	dnf install -y ${i}
 		fi
 	done
+	#fedora neofetch (X)
 	if [[ -n $(command -v zypper) ]]; then
 		printf "%s\n" "${GREEN}zypper ${YELLOW}in -y ${BLUE}ruby2.7-rubygem-lolcat neofetch${RESET}"
 		zypper in -y ruby2.7-rubygem-lolcat
