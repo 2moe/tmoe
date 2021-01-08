a7995cc3c09c00d7ba65ba293c6b9acff21c6f46
2moe <25324935+2moe@users.noreply.github.com>
2021-01-08 04:54:30 +0000

    ELECTRON URL

--- tools/environment.sh
+++ tools/environment.sh
@@ -13,7 +13,7 @@ gnu_linux_env_02() {
     OPT_URL_02='https://dl.bintray.com/debianopt/debianopt'
     OPT_REPO_LIST='/etc/apt/sources.list.d/debianopt.list'
     #ELECTRON_MIRROR_STATION='https://npm.taobao.org/mirrors/electron'
-    ELECTRON_MIRROR_STATION='https://mirrors.huaweicloud.com/electron/'
+    ELECTRON_MIRROR_STATION='https://mirrors.huaweicloud.com/electron'
     TIGER_VNC_DEFAULT_CONFIG_FILE='/etc/tigervnc/vncserver-config-defaults'
     XSESSION_FILE='/etc/X11/xinit/Xsession'
     GIT_AK2='https://gitee.com/ak2'
