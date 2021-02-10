1f67e3d9b7d4f3400b7fa09ed135793d858e2d99
2moe <25324935+2moe@users.noreply.github.com>
2021-02-10 14:55:53 +0800

    runc.deb

--- share/docker-build/amd64/arch/bootstrap.sh
+++ share/docker-build/amd64/arch/bootstrap.sh
@@ -13,16 +13,16 @@ else
     cd ${HOME}/.cache/tmp
 fi
 
+if grep -q 'Ubuntu 20\.04\.' /etc/issue; then
+    git clone --depth=1 https://github.com/cu233/runc.git opencontainers-runc
+    cp -vf opencontainers-runc/*deb ./
+    sudo apt install -y ./*.deb
+fi
+
 sudo bash ${CUR}/arch-bootstrap.sh -a x86_64 arch
 sudo rm -rfv arch/usr/lib/tmpfiles.d/* arch/dev/*
 cd arch
 sudo tar -pcf ${CUR}/arch.tar ./*
-
-if grep -q 'Ubuntu 20\.04\.' /etc/issue; then
-    git clone --depth=1 https://github.com/cu233/runc.git opencontainers-runc
-    cp opencontainers-runc/*deb ./
-    sudo apt install -y ./*.deb
-fi
 
 cd ..
 sudo rm -rf arch
