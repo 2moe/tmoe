27425f71b697a9da66a7efb910eb18467e9d399b
2moe <25324935+2moe@users.noreply.github.com>
2021-02-14 18:19:57 +0800

    kali .zshrc

--- share/docker-build/arm64/arch/bootstrap.sh
+++ share/docker-build/arm64/arch/bootstrap.sh
@@ -16,7 +16,7 @@ fi
 if grep -q 'Ubuntu 20\.04\.' /etc/issue; then
     git clone --depth=1 https://github.com/cu233/runc.git opencontainers-runc
     cp -vf opencontainers-runc/*deb ./
-    sudo apt install -y ./*.deb
+    sudo apt install -y ./runc*.deb
 fi
 
 sudo bash ${CUR}/arch-bootstrap.sh -a aarch64 arch
