efb3cf39ea640c1fcf99bd35591b201109eaa775
2moe <25324935+2moe@users.noreply.github.com>
2021-03-19 18:49:52 +0800

    get_chromium_version

--- tools/app/browser.sh
+++ tools/app/browser.sh
@@ -34,12 +34,16 @@ hold_ubuntu_chromium() {
     apt-mark hold chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg chromium-codecs-ffmpeg-extra
 }
 #########
+get_chromium_version() {
+    CHROMIUM_BROWSER_VERSION="$(printf "%s\n" "${CHROMIUM_DEB_RAW_LIST}" | grep 'chromium-browser_' | grep ${ARCH_TYPE} | grep ${VERSION_ID} | tail -n 1 | cut -d '_' -f 2 | cut -d '-' -f 1)"
+}
 ubuntu_ppa_chromium() {
     if ! grep -q 'development branch' /etc/os-release; then
         VERSION_ID=$(grep 'VERSION_ID=' /etc/os-release | cut -d '"' -f 2)
     else
         VERSION_ID=.
     fi
+    unset CHROMIUM_BROWSER_VERSION
     PPA_REPO_URL="https://packages.tmoe.me/chromium-dev/ubuntu/pool/main/c/chromium-browser/"
     case ${ARCH_TYPE} in
     "amd64" | "arm64")
@@ -52,8 +56,12 @@ ubuntu_ppa_chromium() {
         mkdir -pv ${TEMP_FOLDER}
         cd ${TEMP_FOLDER}
         CHROMIUM_DEB_RAW_LIST=$(curl -L ${PPA_REPO_URL} | grep '\.deb' | egrep -v 'chromium-chromedriver|codecs-ffmpeg_|dbgsym_' | egrep -v "~18\.04\.1_|~16\.04\.1_|\.debian\.tar" | egrep "${ARCH_TYPE}|all\.deb" | awk -F '<a href=' '{print $2}' | cut -d '"' -f 2)
+        get_chromium_version
+        if [[ -z ${CHROMIUM_BROWSER_VERSION} ]]; then
+            VERSION_ID=.
+            get_chromium_version
+        fi
 
-        CHROMIUM_BROWSER_VERSION="$(printf "%s\n" "${CHROMIUM_DEB_RAW_LIST}" | grep 'chromium-browser_' | grep ${ARCH_TYPE} | grep ${VERSION_ID} | tail -n 1 | cut -d '_' -f 2 | cut -d '-' -f 1)"
         CHROMIUM_BROWSER_VERSION_02="$(printf "%s\n" "${CHROMIUM_DEB_RAW_LIST}" | grep 'chromium-browser_' | grep "${CHROMIUM_BROWSER_VERSION}" | grep ${ARCH_TYPE} | grep ${VERSION_ID} | awk -F '~' '{print $NF}' | cut -d '_' -f 1 | tail -n 1)"
 
         CHROMIUM_BROWSER_DEB=$(printf "%s\n" "${CHROMIUM_DEB_RAW_LIST}" | grep 'chromium-browser_' | grep ${CHROMIUM_BROWSER_VERSION} | grep ${CHROMIUM_BROWSER_VERSION_02} | head -n 1)
