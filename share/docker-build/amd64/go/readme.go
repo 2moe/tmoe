f3b2038a9456aa25cab8098ddcef79e34e129c20
2moe <25324935+2moe@users.noreply.github.com>
2021-04-16 13:16:19 +0800

    readme.go

---
+++ share/docker-build/amd64/go/readme.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		country, ProxySite string
+	)
+
+	country = "China"
+	ProxySite = "https://goproxy.cn"
+	fmt.Printf("If you live in\x1b[%dm %v. \x1b[0m\n", 32, country)
+	fmt.Printf("Then you can run the follwing\x1b[%dm commands. \x1b[0m\n", 31)
+
+	fmt.Println("go env -w GO111MODULE=on")
+	fmt.Printf("go env -w GOPROXY=%v,direct\n", ProxySite)
+}
