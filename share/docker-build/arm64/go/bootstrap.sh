47d488374c9c8ef880ba56c2c4a98b632d3d4718
2moe <25324935+2moe@users.noreply.github.com>
2021-07-31 16:55:01 +0800

    GET_GO_VERSION

--- share/docker-build/arm64/go/bootstrap.sh
+++ share/docker-build/arm64/go/bootstrap.sh
@@ -2,3 +2,8 @@
 #####################
 CUR=$(pwd)
 cp ${CUR}/../README.* ${CUR}
+
+URL="https://github.com/docker-library/official-images/raw/master/library/golang"
+# https://github.com/docker-library/docs/raw/master/golang/README.md
+GO_VERSION="$(curl -L ${URL} | grep 'Tags: ' | head -n 1 | awk '{print $NF}')"
+[[ -z ${GO_VERSION} ]] || sed -i -E "s@(/golang:)rc-buster@\1${GO_VERSION}@g" Dockerfile
